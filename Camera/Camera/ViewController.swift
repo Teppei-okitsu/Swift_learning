fab46f5bc9259bbc819c3b6872ca7bebb7f60a0a
Teppei-okitsu <okitsu@h2017192.local>
2020-05-04 13:24:26 +0900

    create camera proect

---
+++ Camera/Camera/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by okitsu teppei on 2020/05/02.
+//  Copyright © 2020 okitsu teppei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true, completion: {() -> Void in
+                print("comlpetion")
+            })
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
