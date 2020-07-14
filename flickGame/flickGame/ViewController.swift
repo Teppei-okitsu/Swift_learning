bdb311d8a88130fefb67d123a1d8f700debbab9e
Teppei-okitsu <okitsu@H2020-289.local>
2020-07-14 22:07:45 +0900

    create NewProject & add asset~create Launch

---
+++ flickGame/flickGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flickGame
+//
+//  Created by okitsu teppei on 2020/07/14.
+//  Copyright © 2020 okitsu teppei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
