b1f6d4f875a5e9d2ce1777ff33dff73c31bd2085
Teppei-okitsu <okitsu@H2020-289.local>
2020-07-19 16:04:29 +0900

    add Quiz

--- flickGame/flickGame/GameViewController.swift
+++ flickGame/flickGame/GameViewController.swift
@@ -12,23 +12,33 @@ import UIKit
 class GameViewController: UIViewController {
 
     @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var quizLabel: UILabel!
+    let quiz = Quiz()
+    var quizList = [""]
+    var quizCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        inputTextField.delegate = self
         print("GameVC_viewDidLoad\n")
+        inputTextField.delegate = self
+        quizList = quiz.setQuizList(level: .easy)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        quizLabel.text = quizList.first
     }
 }
 
 extension GameViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("リターン入力時")
-        if textField.text == "かに" {
-            // キーボードを閉じる
-//            textField.resignFirstResponder()
+        if textField.text == quizLabel.text {
+            quizCount += 1
             textField.text = ""
+            quizLabel.text = quizList[quizCount]
         }
-        // これでも閉じる
+
+        // キーボードを閉じる
+        // textField.resignFirstResponder()
         // textField.endEditing(true)
         return true
     }
